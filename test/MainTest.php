86df8f8a7dbdc3f7832ab5d72a22a753b80558be
borislav <b.manolov@gmail.com>
2012-09-22 08:54:03 +0200

    refactor tests

--- test/MainTest.php
+++ test/MainTest.php
@@ -72,10 +72,109 @@ class MainTest extends TestCase
 		'title-with-note.sfb',
 	);
 
-	public function testConverter()
+	public function testFb2Converter()
 	{
-		foreach ($this->inputFiles as $inputFile) {
-			$this->doTestFile(dirname(__FILE__).'/converter/'.$inputFile);
+		foreach ($this->getInputFiles() as $file) {
+			$fb2File = str_replace('.sfb', '.fb2', $file);
+			//$this->clearFb2File($fb2File);
+			$this->doTestConverter('SfbToFb2Converter', $file, dirname($file), $fb2File, array($this, 'clearFb2String'));
 		}
+	}
+
+	public function testHtmlConverter()
+	{
+		foreach ($this->getInputFiles() as $file) {
+			$htmlFile = str_replace('.sfb', '.html', $file);
+			$this->doTestConverter('SfbToHtmlConverter', $file, 'img', $htmlFile);
+		}
+	}
+
+	private function getInputFiles()
+	{
+		return array_map(function($file){
+			return dirname(__FILE__).'/converter/'.$file;
+		}, $this->inputFiles);
+	}
+
+	private function doTestConverter($converter, $inFile, $imgDir, $outFile, $callback = null)
+	{
+		$converterClass = 'Sfblib_' . $converter;
+		$conv = new $converterClass($inFile, $imgDir);
+		$conv->setObjectCount(1);
+		$conv->rmPattern(' —')->rmRegExpPattern('/^— /');
+		$conv->convert();
+		$testOutput = $conv->getContent();
+		if ( is_callable($callback) ) {
+			$testOutput = call_user_func($callback, $testOutput);
+		}
+		// remove double new lines
+		$testOutput = preg_replace('/\n\n+/', "\n", $testOutput);
+
+		// save output if wanted
+		$outDir = dirname($outFile) . '/output';
+		if (file_exists($outDir)) {
+			file_put_contents($outDir .'/'. basename($outFile), $testOutput);
+		}
+
+		$this->assertEquals(file_get_contents($outFile), $testOutput, "$converter: $inFile");
+	}
+
+
+	private function clearFb2File($file)
+	{
+		if ( ! file_exists($file) ) {
+			return;
+		}
+		$contents = file_get_contents($file);
+		if ( $this->shouldClearFb2String($contents) ) {
+			file_put_contents($file, strtr($contents, array(
+				"\xEF\xBB\xBF" => '', // BOM
+				"\r"    => '',
+				'fn_'  => 'note_',
+			)));
+			$this->removeElementFromFile($file, 'id');
+			$this->removeElementFromFile($file, 'program-used');
+			$this->removeElementFromFile($file, 'date');
+			$this->removeElementFromFile($file, 'stylesheet');
+		}
+	}
+
+
+	private function clearFb2String($string)
+	{
+		if ( $this->shouldClearFb2String($string) ) {
+			$string = $this->removeElementFromString($string, 'id');
+			$string = $this->removeElementFromString($string, 'program-used');
+			$string = $this->removeElementFromString($string, 'date');
+			$string = $this->removeElementFromString($string, 'stylesheet');
+		}
+		return $string;
+	}
+
+
+	private function shouldClearFb2String($string)
+	{
+		return strpos($string, '<id>') !== false;
+	}
+
+
+	private function removeElementFromFile($file, $elm)
+	{
+		$contents = file_get_contents($file);
+		if ( strpos($contents, "<$elm>") !== false ) {
+			file_put_contents($file, $this->removeElementFromString($contents, $elm));
+		}
+	}
+
+
+	private function removeElementFromString($string, $elm)
+	{
+		$start = strpos ( $string, "<$elm" );
+		if ( $start === false ) {
+			return $string;
+		}
+
+		$end = strpos ( $string, "</$elm>", $start ) + strlen("</$elm>");
+		return substr_replace ( $string, '', $start, $end - $start );
 	}
 }
