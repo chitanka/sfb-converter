eb50bb5155027971010d920f5c5fe5a2e4b06b75
borislav <b.manolov@gmail.com>
2013-11-08 07:35:09 +0100

    some progress by fb2 to sfb

--- lib/Sfblib/Fb2ToSfbConverter.php
+++ lib/Sfblib/Fb2ToSfbConverter.php
@@ -23,28 +23,39 @@ class Sfblib_Fb2ToSfbConverter
 		$fb2 = new SimpleXMLElement($data, null, $dataIsFile);
 
 		$sfb = '';
+
+		// TODO stylesheet
+
 		if ($fb2->description) {
-			$sfb .= $this->convertMainTitle($fb2->description);
+			$sfb .= $this->convertMainTitle($fb2->description->{'title-info'});
 			$sfb .= $this->line();
+			// TODO description
 		}
 		if ($fb2->body->image) {
 			$sfb .= $this->convertImage($fb2->body->image);
 		}
+
+		// skip $fb2->body->title
+
 		if ($fb2->body->epigraph) {
 			$sfb .= $this->convertEpigraphs($fb2->body->epigraph);
 		}
 		if ($fb2->body->section) {
 			$sfb .= $this->convertSections($fb2->body->section);
 		}
+
+		if ($fb2->body->binary) {
+			$this->convertBinaries($fb2->body->binary);
+		}
+
 		$sfb = $this->clearSfb($sfb);
 
 		return $sfb;
 	}
 
-	private function convertMainTitle(SimpleXMLElement $description)
+	private function convertMainTitle(SimpleXMLElement $titleInfo)
 	{
 		$sfb = '';
-		$titleInfo = $description->{'title-info'};
 		$sfb .= $this->line($this->convertMainAuthor($titleInfo->author), '|');
 		$sfb .= $this->line($titleInfo->{'book-title'}, '|');
 		return $sfb;
@@ -57,6 +68,91 @@ class Sfblib_Fb2ToSfbConverter
 			//$author->{'middle-name'},
 			$author->{'last-name'}
 		));
+		// <nickname> - 0..1 (один, обязателен при отсутствии <first-name> и <last-name>, иначе опционально); 
+	}
+
+	// TODO
+	private function convertDescription()
+	{
+//<src-title-info> - 0..1 (один, опционально) с версии 2.1;
+//<document-info> - 1 (один, обязателен);
+//<publish-info> - 0..1 (один, опционально);
+//<custom-info> - 0..n (любое число, опционально);
+//<output> - 0..2
+	}
+
+	// TODO
+	private function convertTitleInfo()
+	{
+//<genre> - 1..n (любое число, один обязaтелен);
+//<author> - 1..n (любое число, один обязaтелен);
+//<book-title> - 1 (один, обязателен);
+//<annotation> - 0..1 (один, опционально);
+//<keywords> - 0..1 (один, опционально);
+//<date> - 0..1 (один, опционально);
+//<coverpage> - 0..1 (один, опционально);
+//<lang> - 1 (один, обязателен);
+//<src-lang> - 0..1 (один, опционально);
+//<translator> - 0..n (любое число, опционально);
+//<sequence> - 0..n (любое число, опционально). <sequence name="Грегор Эйзенхорн" number="2"/>
+	}
+
+	// TODO
+	private function convertSrcTitleInfo()
+	{
+//<genre> - 1..n (любое число, один обязaтелен);
+//<author> - 1..n (любое число, один обязaтелен);
+//<book-title> - 1 (один, обязателен);
+//<annotation> - 0..1 (один, опционально);
+//<keywords> - 0..1 (один, опционально);
+//<date> - 0..1 (один, опционально);
+//<coverpage> - 0..1 (один, опционально);
+//<lang> - 1 (один, обязателен);
+//<src-lang> - 0..1 (один, опционально);
+//<translator> - 0..n (любое число, опционально);
+//<sequence> - 0..n (любое число, опционально).
+	}
+
+	// TODO
+	private function convertDocumentInfo()
+	{
+//<author> - 1..n (любое число, один обязaтелен);
+//<program-used> - 0..1 (один, опционально);
+//<date> - 1 (один, обязателен);
+//<src-url> - 0..n (любое число, опционально); източник
+//<src-ocr> - 0..1 (один, опционально);
+//<id> - 1 (один, обязателен);
+//<version> - 1 (один, обязателен);
+//<history> - 0..1 (один, опционально);
+//<publisher> - 0..n (любое число, опционально) с версии 2.2.
+	}
+
+	// TODO
+	private function convertPublishInfo()
+	{
+//<book-name> - 0..1 (один, опционально) - название;
+//<publisher> - 0..1 (один, опционально) - издательство;
+//<city> - 0..1 (один, опционально)- место издания;
+//<year> - 0..1 (один, опционально) - год издания;
+//<isbn> - 0..1 (один, опционально) - ISBN издания;
+//<sequence> - 0..n (любое число, опционально) - серия (серии) изданий, в которую входит книга.
+	}
+
+	// TODO
+	private function convertAnnotation()
+	{
+//<p>;
+//<poem>;
+//<cite>;
+//<subtitle>;
+//<empty-line>;
+//<table> (с версии 2.1).
+	}
+
+	// TODO
+	private function convertCoverpage()
+	{
+		// <coverpage><image l:href="#cover.jpg"/></coverpage>
 	}
 
 	private function convertImage(SimpleXMLElement $image)
@@ -89,10 +185,12 @@ class Sfblib_Fb2ToSfbConverter
 					$sfb .= $this->convertParagraph($elm); break;
 				case 'poem':
 					$sfb .= $this->convertPoem($elm); break;
-				case 'subtitle':
-					$sfb .= $this->convertSubtitle($elm); break;
+				case 'cite':
+					$sfb .= $this->convertCite($elm); break;
 				case 'empty-line':
 					$sfb .= $this->line(); break;
+				case 'text-author':
+					$sfb .= $this->convertTextAuthor($elm); break;
 			}
 		}
 		$sfb .= $this->command('E$');
@@ -117,26 +215,27 @@ class Sfblib_Fb2ToSfbConverter
 		$sfb .= $this->convertTitle($section->title, $level);
 		$sfb .= $this->convertEpigraphs($section->epigraph);
 		$sfb .= $this->convertImage($section->image);
+		//$sfb .= $this->convertAnnotation($section->annotation);
 		if ($section->section) {
 			$sfb .= $this->convertSections($section->section, $level + 1);
-		} else {
-			foreach ($section->children() as $elm) {
-				switch ($elm->getName()) {
-					case 'p':
-						$sfb .= $this->convertParagraph($elm); break;
-					case 'image':
-						$sfb .= $this->convertImage($elm); break;
-					case 'poem':
-						$sfb .= $this->convertPoem($elm); break;
-					case 'subtitle':
-						$sfb .= $this->convertSubtitle($elm); break;
-					case 'cite':
-						$sfb .= $this->convertCite($elm); break;
-					case 'empty-line':
-						$sfb .= $this->line(); break;
-					case 'table':
-						$sfb .= $this->convertTable($elm); break;
-				}
+			return $sfb;
+		}
+		foreach ($section->children() as $elm) {
+			switch ($elm->getName()) {
+				case 'p':
+					$sfb .= $this->convertParagraph($elm); break;
+				case 'image':
+					$sfb .= $this->convertImage($elm); break;
+				case 'poem':
+					$sfb .= $this->convertPoem($elm); break;
+				case 'subtitle':
+					$sfb .= $this->convertSubtitle($elm); break;
+				case 'cite':
+					$sfb .= $this->convertCite($elm); break;
+				case 'empty-line':
+					$sfb .= $this->line(); break;
+				case 'table':
+					$sfb .= $this->convertTable($elm); break;
 			}
 		}
 		return $sfb;
@@ -152,13 +251,14 @@ class Sfblib_Fb2ToSfbConverter
 	private function convertTitle(SimpleXMLElement $title, $level = 1)
 	{
 		$sfb = '';
-		if ($title) {
-			$sfb .= $this->line();
-			foreach ($title->p as $paragraph) {
-				$sfb .= $this->convertParagraph($paragraph, $this->_titleMarkers[$level]);
-			}
-			$sfb .= $this->line();
+		if (!$title) {
+			return $sfb;
 		}
+		$sfb .= $this->line();
+		foreach ($title->p as $paragraph) {
+			$sfb .= $this->convertParagraph($paragraph, $this->_titleMarkers[$level]);
+		}
+		$sfb .= $this->line();
 		return $sfb;
 	}
 
@@ -189,8 +289,6 @@ class Sfblib_Fb2ToSfbConverter
 					$sfb .= $this->convertAuthor($elm); break;
 				case 'date':
 					$sfb .= $this->convertDate($elm); break;
-				case 'empty-line':
-					$sfb .= $this->line(); break;
 			}
 		}
 		$sfb .= $this->command('P$');
@@ -207,11 +305,35 @@ class Sfblib_Fb2ToSfbConverter
 	private function convertCite(SimpleXMLElement $cite)
 	{
 		$sfb = '';
-		$sfb .= $cite->asXML();
+		$sfb .= $this->command('C>');
+		foreach ($cite->children() as $elm) {
+			switch ($elm->getName()) {
+				case 'p':
+					$sfb .= $this->convertParagraph($elm); break;
+				case 'subtitle':
+					$sfb .= $this->convertSubtitle($elm); break;
+				case 'empty-line':
+					$sfb .= $this->line(); break;
+				case 'poem':
+					$sfb .= $this->convertPoem($elm); break;
+				case 'table':
+					$sfb .= $this->convertTable($elm); break;
+				case 'text-author':
+					$sfb .= $this->convertTextAuthor($elm); break;
+			}
+		}
+		$sfb .= $this->command('C$');
 		return $sfb;
 	}
 
-	// TODO
+	private function convertTextAuthor(SimpleXMLElement $textAuthor)
+	{
+		$sfb = '';
+		$sfb .= $this->line($this->convertInlineElements($textAuthor->asXML()), '@');
+		return $sfb;
+	}
+
+	// TODO td, th, tr
 	private function convertTable(SimpleXMLElement $table)
 	{
 		$sfb = '';
@@ -260,6 +382,12 @@ class Sfblib_Fb2ToSfbConverter
 		$sfb = preg_replace("|$reStart{s}(.+){/s}$reEnd|U", '__$1__', $sfb);
 		$sfb = ltrim($sfb, ' ');
 		return $sfb;
+	}
+
+	// TODO
+	private function convertBinaries(SimpleXMLElement $binaries)
+	{
+
 	}
 
 	private function line($content = '', $command = '')
